c298e98b62bec417249a6c361787434586f87231
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-04-25 00:20:26 +0300

    added drawSelection functionality

--- chess/scripts/drawBoard.js
+++ chess/scripts/drawBoard.js
@@ -1,11 +1,20 @@
-function drawBoard(board) {
-	let $gameCanvas = $('#game-canvas');
-	let $gameCanvasContext = $gameCanvas[0].getContext('2d');
-	let $backgroundImg = $('#background');
+function drawBoard(board, selectionCoords) {
+	const $gameCanvas = $('#game-canvas');
 	const POS_STEP = ($gameCanvas[0].clientWidth)/8;
-	
+	const $gameCanvasContext = $gameCanvas[0].getContext('2d');
+	const $backgroundImg = $('#background');
+	const $selectionImg = $('#selection');
+	let selectionX;
+	let selectionY;
+
 	$gameCanvasContext.drawImage($backgroundImg[0], 0, 0);
 	
+	if (selectionCoords){
+		selectionX = selectionCoords.x * POS_STEP;
+		selectionY = selectionCoords.y * POS_STEP;
+		$gameCanvasContext.drawImage($selectionImg[0], selectionX, selectionY);
+	}
+
 	for (let i = 0; i < board.length; i += 1){
 		for (let j = 0; j < board[i].length; j += 1){
 			if (board[i][j]){
@@ -19,8 +28,7 @@ function drawBoard(board) {
 
 		$gameCanvasContext.drawImage(
 			$(imgselector)[0], 
-			((piece.posX-1) * POS_STEP), 
-			((piece.posY-1) * POS_STEP));
+			((piece.posX) * POS_STEP), 
+			((piece.posY) * POS_STEP));
 	}
 }
-
