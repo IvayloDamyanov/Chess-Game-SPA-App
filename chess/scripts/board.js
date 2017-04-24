5a1e8c6a1fe8fad4bffc8bf3ceec69ab0cd2afd6
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-04-24 21:32:39 +0300

    Update board.js

--- chess/scripts/board.js
+++ chess/scripts/board.js
@@ -1,5 +1,29 @@
 class Board {
     constructor(){
-        this._field = [8][8];
+        this.board = [[],[],[],[],[],[],[],[]];
+    }
+
+    get board(){
+        return this._board;
+    }
+
+    set board(value){
+        this._board = value;
+    }
+
+    initializeBoard(){
+        let pieces = [
+            new King("white", 1, 1),
+            new King("black", 8, 1),
+            new King("white", 3, 4)
+        ]
+        
+        this.populateBoard(pieces);
+    }
+
+
+
+    populateBoard(pieces){
+        pieces.forEach((piece) => this.board[piece.posX-1][piece.posY-1] = piece);
     }
 }
