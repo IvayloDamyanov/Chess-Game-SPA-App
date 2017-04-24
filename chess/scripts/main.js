5c8b9efbfc083e53c6502ef0c09a9e67b2d9c11a
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-04-25 00:15:51 +0300

    Update main.js

--- chess/scripts/main.js
+++ chess/scripts/main.js
@@ -1,29 +1,42 @@
 window.addEventListener('load', function () {
 	const CANVAS_SIZE = 512;
-
-	$body = $("body");
-	$body.append($("<div />")
-		.attr("id", "game-canvas-container")
-		.css("width", CANVAS_SIZE+"px")
-		.css("height", CANVAS_SIZE+"px")
-		.css("margin", "0px auto")
-		.css("border", "2px solid black")
-	);
-	$gameCanvasContainer = $("#game-canvas-container");
-	$gameCanvasContainer.append($('<canvas />')
-		.attr("id", "game-canvas")
-		.attr("width", CANVAS_SIZE+"px") //don't change to .css, breaks visualization
-		.attr("height", CANVAS_SIZE+"px") //don't change to .css, breaks visualization
-	);
-
+	
 	let gameBoard = new Board;	
 	gameBoard.initializeBoard();
+	drawBoard(gameBoard.board);
 	
-	function gameLoop() {
-		drawBoard(gameBoard.board);
-		
-		//window.requestAnimationFrame(gameLoop);
-	}
+	var $canvas = $('#game-canvas');
 
-	gameLoop();
+    $canvas.on("click", function(evt) {
+        let mousePos = getMousePos($canvas, evt); //mouse position in pixels
+		let selectionCoords = getCoords(mousePos); //returns xPos and yPos of of selection
+		console.log(selectionCoords.x + ", " +selectionCoords.y);
+		
+		drawBoard(gameBoard.board, selectionCoords);
+    });
+
+    
+	// $body = $("body");
+	// $body.append($("<div />")
+	// 	.attr("id", "game-canvas-container")
+	// 	.css("width", CANVAS_SIZE+"px")
+	// 	.css("height", CANVAS_SIZE+"px")
+	// 	.css("margin", "0px auto")
+	// 	.css("border", "2px solid black")
+	// );
+	// $gameCanvasContainer = $("#game-canvas-container");
+	// $gameCanvasContainer.append($('<canvas />')
+	// 	.attr("id", "game-canvas")
+	// 	.attr("width", CANVAS_SIZE+"px") //don't change to .css, breaks visualization
+	// 	.attr("height", CANVAS_SIZE+"px") //don't change to .css, breaks visualization
+	// );
+
+
+	// function gameLoop() {
+	// 	drawBoard(gameBoard.board);
+		
+	// 	window.requestAnimationFrame(gameLoop);
+	// }
+
+	// gameLoop();
 });
