9952cc5a7c4f868adb2ffd87a88034b7383305f1
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-04-25 05:19:34 +0300

    Added package.json file

---
+++ public/scripts/board.js
@@ -0,0 +1,66 @@
+class Board {
+    constructor(){
+        this.board = [[],[],[],[],[],[],[],[]];
+    }
+
+    get board(){
+        return this._board;
+    }
+
+    set board(value){
+        this._board = value;
+    }
+
+    initializeBoard(){
+        let pieces = [
+            new King("white", 0, 0),
+            new King("white", 1, 1),
+            new King("black", 6, 6),
+            new King("black", 7, 7)
+        ]
+        
+        // uncomment the following starting positions when game is ready
+        // pieces = [
+        //     new Pawn("white", 1, 7),
+        //     new Pawn("white", 2, 7),
+        //     new Pawn("white", 3, 7),
+        //     new Pawn("white", 4, 7),
+        //     new Pawn("white", 5, 7),
+        //     new Pawn("white", 6, 7),
+        //     new Pawn("white", 7, 7),
+        //     new Pawn("white", 8, 7),
+        //     new Rook("white", 1, 8),
+        //     new Rook("white", 8, 8),
+        //     new Knight("white", 2, 8),
+        //     new Knight("white", 7, 8),
+        //     new Bishop("white", 3, 8),
+        //     new Bishop("white", 6, 8),
+        //     new Queen("white", 4, 8),
+        //     new King("white", 5, 8),
+        //     new Pawn("black", 1, 2),
+        //     new Pawn("black", 2, 2),
+        //     new Pawn("black", 3, 2),
+        //     new Pawn("black", 4, 2),
+        //     new Pawn("black", 5, 2),
+        //     new Pawn("black", 6, 2),
+        //     new Pawn("black", 7, 2),
+        //     new Pawn("black", 8, 2),
+        //     new Rook("black", 1, 1),
+        //     new Rook("black", 8, 1),
+        //     new Knight("black", 2, 1),
+        //     new Knight("black", 7, 1),
+        //     new Bishop("black", 3, 1),
+        //     new Bishop("black", 6, 1),
+        //     new Queen("black", 4, 1),
+        //     new King("black", 5, 1)
+        // ]
+        
+        this.populateBoard(pieces);
+    }
+
+    
+
+    populateBoard(pieces){
+        pieces.forEach((piece) => this.board[piece.posX][piece.posY] = piece);
+    }
+}
---
+++ public/scripts/pieces.js
@@ -0,0 +1,187 @@
+class Piece{
+    constructor(color, posX, posY){
+        this.color = color;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    set color(value){
+        if (!(value == "white" || value == "black")){
+            throw Error("invalid color");
+        }
+        this._color = value;
+    }
+
+    get posX(){
+        return this._posX;
+    }
+
+    set posX(value){
+        if (typeof(value) != "number"){
+            throw new Error("posX is not a number");
+        }
+        if ((value < 0 || 7 < value) || value%1 !== 0){
+            throw new Error("invalid posX value");
+        }
+        this._posX = value;
+    }
+
+    get posY(){
+        return this._posY;
+    }
+
+    set posY(value){
+        if (typeof(value) != "number"){
+            throw new Error("posY is not a number");
+        }
+        if ((value < 0 || 7 < value) || value%1 !== 0){
+            throw new Error("invalid posY value");
+        }
+        this._posY = value;
+    }
+
+    get moves(){
+        return this._moves;
+    }
+
+    set moves(value){
+        if (typeof(value) !== "object"){
+            throw new Error("List of moves is not an object")
+        }
+        this._moves = value;
+    }
+
+    checkBounds(newX, newY){
+        if(newX < 0 || 7 < newX || newY < 0 || 7 < newY){
+            return false;
+        }
+        return true;
+    }
+
+    checkMove(newX, newY, moves){
+        let xMove = newX - this.posX;
+        let yMove = newY - this.posY;
+        for(let i = 0; i < moves.length; i += 1){
+            if(moves[i][0] == xMove && moves[i][1] == yMove){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class King extends Piece{
+    constructor(color, posX, posY){
+        super(color, posX, posY);
+            this.moves = [
+                [0, -1],
+                [1, -1],
+                [1, 0],
+                [1, 1],
+                [0, 1],
+                [-1, 1],
+                [-1, 0],
+                [-1, -1]
+            ];
+            this.type = "King";
+            this.isMoved = false;
+    }
+    get moves(){
+        return this._moves;
+    }
+
+    set moves(value){
+        this._moves = value;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(value){
+        if (value !== "King"){
+            throw new Error("Not a King piece type");
+        }
+        this._type = value;
+    }
+
+    get isMoved(){
+        return this._isMoved;
+    }
+
+    set isMoved(value){
+        this._isMoved = value;
+    }
+
+    move(board, newX, newY){
+        if((typeof(board[newX, newY]) !== "undefined") && board[newX, newY].color === this.color){
+            return false;
+        }
+
+        if(!(super.checkBounds(newX, newY))){
+            return false;
+        }
+
+        if(!(super.checkMove(newX, newY, this.moves))){
+            return false;
+        }
+        
+        return true;
+    }    
+}
+
+class Knight extends Piece{
+    constructor(color, posX, posY){
+        super(color, posX, posY);
+        this.moves = [
+            [-1, -2],
+            [-2, -1],
+            [-2, 1],
+            [-1, 2],
+            [1, 2],
+            [2, 1],
+            [2, -1],
+            [1, -2]
+        ];
+        this.type = "Knight";
+    }
+
+    get moves(){
+        return this._moves;
+    }
+
+    set moves(value){
+        this._moves = value;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(value){
+        if (value !== "Knight"){
+            throw new Error("Not a Knight piece type");
+        }
+        this._type = value;
+    }
+
+    move(board, newX, newY){
+        if((typeof(board[newX, newY]) !== "undefined") && board[newX, newY].color === this.color){
+            return false;
+        }
+
+        if(!(super.checkBounds(newX, newY))){
+            return false;
+        }
+
+        if(!(super.checkMove(newX, newY, this.moves))){
+            return false;
+        }
+
+        return true;
+    }
+}
