14c507ca9172430f4d6a857872b4440aaedb139e
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-05-08 02:18:28 +0300

    Adding Player class

---
+++ scripts/player.js
@@ -0,0 +1,37 @@
+class Player {
+    constructor(color, name){
+        this.color = color;
+        this.name = name;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    set color(value){
+        if (!(value == "white" || value == "black")){
+            throw Error("invalid color");
+        }
+        this._color = value;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(value){
+        let playerNameRegex = /^[a-zA-Z0-9]+$/;
+        if (!value.match(playerNameRegex)){
+            throw new Error("Player name contains invalid symbols");
+        }
+        this._name = value;
+    }
+}
+
+function swapPlayer(playerOnMove, player1, player2){
+    if(playerOnMove.color == player1.color){
+        return player2;
+    } else {
+        return player1;
+    }
+}
