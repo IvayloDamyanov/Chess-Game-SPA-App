c52a1a129d662711a5a61504517251dd2e44d31b
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-05-07 10:01:24 +0300

    Added functions for user signIn and user signUp

--- scripts/controllers/user-controller.js
+++ scripts/controllers/user-controller.js
@@ -15,6 +15,67 @@ let userController = (function() {
             }
         })
     }
+
+    function loadInitialPage(sammy) {
+        return userDatabase.getCurrentUser()
+            .then(user => {
+                if (user) {
+                    sammy.redirect('#/home');
+                } else {
+                    pageLoader.loadPage('initial-page', $mainContainer);
+                }
+            });
+    }
+
+    function loadSignupPage() {
+        return pageLoader.loadPage('user-signup', $mainContainer);
+    }
+
+    function loadSignInPage() {
+        return pageLoader.loadPage('user-login', $mainContainer);
+    }
+
+    function loadHomePage() {
+        return pageLoader.loadPage('header', $headerContainer)
+            .then(_ => {
+                $mainContainer.html('');
+            });
+    }
+
+    function signIn(sammy) {
+        let email = sammy.params.email,
+            password = sammy.params.password;
+
+        userModel
+            .signIn(email, password)
+            .then(_ => {
+                return new Promise(function(resolve, reject) {
+                    toastr.success(`Hi, welcome to our Chess Game Site`);
+                    sammy.redirect('#/home');
+                    resolve();
+                })
+            })
+            .catch(err => {
+                toastr.error(err.message);
+            });
+    }
+
+    function signUp(sammy) {
+        let email = sammy.params.email,
+            password = sammy.params.password;
+
+        userModel.signUp(email, password)
+            .then(_ => {
+                return new Promise(function(resolve, reject) {
+                    toastr.success('You succesfully logged out!')
+                    sammy.redirect('#/login');
+                    resolve();
+                })
+            })
+            .catch(err => {
+                toastr.error(err.message);
+            })
+    }
 }());
 
 export default userController;
