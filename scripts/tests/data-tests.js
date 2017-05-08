e1d33d47999e450ae7bc261c307b9811fe578fd1
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-05-09 00:16:14 +0300

    Added King tests

--- scripts/tests/data-tests.js
+++ scripts/tests/data-tests.js
@@ -122,5 +122,70 @@ describe('Chess Logic Tests', () => {
                 expect(result).to.equal(false);
             });
         });
+
+        describe('King Tests', () => {
+            it('expect King to be constructed properly', () => {
+                const king = new King("black", 4, 7);
+                const typeOfFigure = "King";
+                const moves = [];
+                const standardMoves = [
+                    [1, -1],
+                    [1, 0],
+                    [1, 1],
+                    [0, 1],
+                    [-1, 1],
+                    [-1, 0],
+                    [-1, -1],
+                    [0, -1]
+                ];
+
+                expect(king.type).to.equal(typeOfFigure);
+                expect(king.isMoved).to.equal(false);
+                expect(king.moves).to.deep.equal(moves);
+                expect(king.standartMoves).to.deep.equal(standardMoves);
+            });
+
+            it('expect getMoves method of King to add [2, 0] and [-2, 0] move  to array when the figure is white', () => {
+                const king = new King("black", 4, 0);
+                const rook = new Rook("black", 7, 7);
+                const gameBoard = new Board();
+
+                gameBoard.board[7][0] = rook;
+                gameBoard.board[0][0] = rook;
+
+                king.getMoves(gameBoard.board);
+
+                expect(king.moves).to.have.length(10);
+            });
+
+            it('expect move to return true', () => {
+                const king = new King("black", 4, 0);
+                const rook = new Rook("black", 7, 7);
+                const gameBoard = new Board();
+
+                gameBoard.board[7][0] = rook;
+                gameBoard.board[0][0] = rook;
+
+                const result = king.move(gameBoard.board, 5, 1);
+
+                expect(king.posX).to.equal(5);
+                expect(king.posY).to.equal(1);
+                expect(king.isMoved).to.equal(true);
+                expect(result).to.equal(true);
+            });
+
+            it('expect move to return false', () => {
+                const king = new King("black", 4, 0);
+                const rook = new Rook("black", 7, 7);
+                const gameBoard = new Board();
+
+                gameBoard.board[7][0] = rook;
+                gameBoard.board[0][0] = rook;
+
+                const result = king.move(gameBoard.board, 6, 1);
+
+                expect(result).to.equal(false);
+            });
+        });
     });
 });
