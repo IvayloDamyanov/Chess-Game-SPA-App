fcec06944c6b3e238a7fb52af72f79eed2c2bdcb
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-05-09 03:24:33 +0300

    Minor gamelogic fix

--- scripts/gameLogic/startGame.js
+++ scripts/gameLogic/startGame.js
@@ -1,36 +1,46 @@
 $(window).ready(function () {
-	const CANVAS_SIZE = 512;
-	
-	let gameBoard = new Board();	
-	gameBoard.initializeBoard();
-	drawBoard(gameBoard.board);
-	
-	var $canvas = $('#game-canvas');
-	let selectedPiece;
-	let player1 = new Player("white", "Whitey");
-	let player2 = new Player("black", "Blacky");
-	let player = player1;
-	let winner = false;
-
-    $canvas.on("click", function(evt) {
-        let mousePos = getMousePos($canvas, evt); //mouse position in pixels
-		let selectionCoords = getCoords(mousePos); //returns xPos and yPos of of selection
-		if (selectedPiece && (player.color == selectedPiece.color)){
-			let oldX = selectedPiece.posX;
-			let oldY = selectedPiece.posY;
-			if (selectedPiece.move(gameBoard.board, selectionCoords.x, selectionCoords.y)){
-				gameBoard.clearField(oldX, oldY);
-				gameBoard.movePiece(gameBoard.board, selectedPiece);
-				player = swapPlayer(player, player1, player2);
-			}
-			selectedPiece = false;
-			winner = gameBoard.checkGameOver(gameBoard.board, player1, player2);
-			if(winner){
-				alert(winner.name + " wins");
-			}
-		} else if (gameBoard.checkField(selectionCoords.x, selectionCoords.y)){
-			selectedPiece = gameBoard.selectPiece(selectionCoords.x, selectionCoords.y);
+	var promise = new Promise(function(resolve, reject){
+		if(true){
+			setTimeout(resolve, 300);
 		}
-		drawBoard(gameBoard.board, selectionCoords);
-    });
+	});
+
+	promise.then(function(){
+		const CANVAS_SIZE = 512;
+
+		let gameBoard = new Board();
+		let player1 = new Player("white", "Whitey");
+		let player2 = new Player("black", "Blacky");
+			
+		gameBoard.initializeBoard();
+		drawBoard(gameBoard.board);
+		
+		var $canvas = $('#game-canvas');
+		let selectedPiece;
+		
+		let player = player1;
+		let winner = false;
+
+		$canvas.on("click", function(evt) {
+			let mousePos = getMousePos($canvas, evt); //mouse position in pixels
+			let selectionCoords = getCoords(mousePos); //returns xPos and yPos of of selection
+			if (selectedPiece && (player.color == selectedPiece.color)){
+				let oldX = selectedPiece.posX;
+				let oldY = selectedPiece.posY;
+				if (selectedPiece.move(gameBoard.board, selectionCoords.x, selectionCoords.y)){
+					gameBoard.clearField(oldX, oldY);
+					gameBoard.movePiece(gameBoard.board, selectedPiece);
+					player = swapPlayer(player, player1, player2);
+				}
+				selectedPiece = false;
+				winner = gameBoard.checkGameOver(gameBoard.board, player1, player2);
+				if(winner){
+					alert(winner.name + " wins");
+				}
+			} else if (gameBoard.checkField(selectionCoords.x, selectionCoords.y)){
+				selectedPiece = gameBoard.selectPiece(selectionCoords.x, selectionCoords.y);
+			}
+			drawBoard(gameBoard.board, selectionCoords);
+		});
+	});
 });
