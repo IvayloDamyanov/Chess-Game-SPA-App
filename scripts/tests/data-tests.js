629728337ecad090489f4af69570fa224968b933
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-05-09 11:27:30 +0300

    Finished testing the chess

--- scripts/tests/data-tests.js
+++ scripts/tests/data-tests.js
@@ -326,4 +326,208 @@ describe('Chess Logic Tests', () => {
             });
         });
     });
+
+    describe('Board tests', () => {
+        it('expect to initialize board correctly', () => {
+            const board = new Board();
+
+            board.initializeBoard();
+
+            const pieces = [
+                new Pawn("white", 0, 6),
+                new Pawn("white", 1, 6),
+                new Pawn("white", 2, 6),
+                new Pawn("white", 3, 6),
+                new Pawn("white", 4, 6),
+                new Pawn("white", 5, 6),
+                new Pawn("white", 6, 6),
+                new Pawn("white", 7, 6),
+                new Rook("white", 0, 7),
+                new Rook("white", 7, 7),
+                new Knight("white", 1, 7),
+                new Knight("white", 6, 7),
+                new Bishop("white", 2, 7),
+                new Bishop("white", 5, 7),
+                new Queen("white", 3, 7),
+                new King("white", 4, 7),
+                new Pawn("black", 0, 1),
+                new Pawn("black", 1, 1),
+                new Pawn("black", 2, 1),
+                new Pawn("black", 3, 1),
+                new Pawn("black", 4, 1),
+                new Pawn("black", 5, 1),
+                new Pawn("black", 6, 1),
+                new Pawn("black", 7, 1),
+                new Rook("black", 0, 0),
+                new Rook("black", 7, 0),
+                new Knight("black", 1, 0),
+                new Knight("black", 6, 0),
+                new Bishop("black", 2, 0),
+                new Bishop("black", 5, 0),
+                new Queen("black", 3, 0),
+                new King("black", 4, 0)
+            ];
+
+            expect(board.pieces).to.deep.equal(pieces);
+        });
+
+        it('expect check field method to return true', () => {
+            const board = new Board();
+
+            board.initializeBoard();
+
+            const result = board.checkField(4, 0);
+
+            expect(result).to.equal(true);
+        });
+
+        it('expect check field method to return false', () => {
+            const board = new Board();
+
+            board.initializeBoard();
+
+            const result = board.checkField(4, 4);
+
+            expect(result).to.equal(false);
+        });
+
+        it('expect selectPiece to return the selected piece', () => {
+            const board = new Board();
+            const king = new King("black", 4, 0);
+
+            board.initializeBoard();
+
+            const result = board.selectPiece(4, 0);
+
+            expect(result).to.deep.equal(king);
+        });
+
+        it('expect ClearField to clear the selected piece', () => {
+            const board = new Board();
+            const king = new King("black", 4, 0);
+
+            board.initializeBoard();
+
+            board.clearField(4, 0);
+
+            const result = board.board[4][0];
+
+            expect(result).to.equal(false);
+        });
+
+        it('expect movePiece to move the selected piece', () => {
+            const board = new Board();
+            const bishop = new Bishop("black", 2, 0);
+
+            board.initializeBoard();
+
+            board.movePiece(board.board, bishop);
+
+            const result = board.board[2][0];
+
+            expect(result).to.deep.equal(bishop);
+        });
+
+        it('expect pieceUpgrade to upgrade the selected piece', () => {
+            const board = new Board();
+            const bishop = new Bishop("black", 2, 0);
+            const queen = new Queen("black", 2, 0);
+
+            board.initializeBoard();
+
+            board.pieceUpgrade(board.board, bishop);
+
+            const result = board.board[2][0];
+
+            expect(result).to.deep.equal(queen);
+        });
+
+        it('expect checkUpgrade to work correctly with black pawn', () => {
+            const board = new Board();
+            const pawn = new Pawn("black", 2, 7);
+            const queen = new Queen("black", 2, 7);
+
+            board.initializeBoard();
+
+            board.checkUpgrade(board.board, pawn);
+
+            const result = board.board[2][7];
+
+            expect(result).to.deep.equal(queen);
+        });
+
+        it('expect checkUpgrade to work correctly with white pawn', () => {
+            const board = new Board();
+            const pawn = new Pawn("white", 2, 0);
+            const queen = new Queen("white", 2, 0);
+
+            board.initializeBoard();
+
+            board.checkUpgrade(board.board, pawn);
+
+            const result = board.board[2][0];
+
+            expect(result).to.deep.equal(queen);
+        });
+
+        it('expect checkGameOver to return false when hasWhiteKing and hasBlackKing', () => {
+            const board = new Board();
+            const player1 = new Player("white", "Gosho");
+            const player2 = new Player("black", "Pesho");
+            const whiteKing = new King("white", 3, 0);
+            const blackKing = new King("black", 3, 0);
+
+            board.initializeBoard();
+
+            board.board[3][0] = whiteKing;
+            board.board[3][0] = blackKing;
+
+            const result = board.checkGameOver(board.board, player1, player2);
+
+            expect(result).to.equal(false);
+        });
+
+        it('expect checkGameOver to return player1', () => {
+            const board = new Board();
+            const player1 = new Player("white", "Gosho");
+            const player2 = new Player("black", "Pesho");
+
+            board.initializeBoard();
+
+            board.board[4][0] = false;
+
+            const result = board.checkGameOver(board.board, player1, player2);
+
+            expect(result).to.deep.equal(player1);
+        });
+
+        it('expect checkGameOver to return player2', () => {
+            const board = new Board();
+            const player1 = new Player("white", "Gosho");
+            const player2 = new Player("black", "Pesho");
+
+            board.initializeBoard();
+
+            board.board[4][7] = false;
+
+            const result = board.checkGameOver(board.board, player1, player2);
+
+            expect(result).to.deep.equal(player2);
+        });
+
+        it('expect checkGameOver to return false when hasWhiteKing and hasBlackKing are false ', () => {
+            const board = new Board();
+            const player1 = new Player("white", "Gosho");
+            const player2 = new Player("black", "Pesho");
+
+            board.initializeBoard();
+
+            board.board[4][0] = false;
+            board.board[4][7] = false;
+
+            const result = board.checkGameOver(board.board, player1, player2);
+
+            expect(result).to.equal(false);
+        });
+    });
 });
